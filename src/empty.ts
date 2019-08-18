0ca60a7e60761496316655dae88a749b201994c1
R.G. Wood <ric@grislyeye.com>
2019-08-18 19:08:33 +0100

    Fix: should filter out empty GM messages

--- src/empty.ts
+++ src/empty.ts
@@ -1,4 +1,4 @@
-import { Message, PlayerMessage, PlayerEvent } from './messages'
+import { Message, PlayerMessage, GameMasterMessage, PlayerEvent } from './messages'
 
 const emptyMessage: (event: PlayerEvent) => boolean = (event: PlayerEvent) =>
   (event.message !== undefined && event.message.trim() !== '')
@@ -7,13 +7,15 @@ export const empty: (messages: Message[]) => Message[] = (messages: Message[]) =
   const filtered = messages.map(message => {
     switch(message.kind) {
       case "player": return new PlayerMessage(message.actor, message.events.filter(emptyMessage))
+      case "gm": return new GameMasterMessage(message.events.filter(emptyMessage))
       default: return message
     }
   })
 
   return filtered.filter(message => {
     switch(message.kind) {
-      case "player": return message.events.length !== 0
+      case "player":
+      case "gm": return message.events.length !== 0
       default: return true
     }
   })
